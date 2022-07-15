ac3b0209ce2b33a4704b5238dc403f0c93f29e90
Gustavo Becelli <gustavobecelli@gmail.com>
2022-07-15 15:52:52 -0300

    Extend support for Windows, Mac and Linux

--- project.py
+++ project.py
@@ -1,40 +1,43 @@
-from dataclasses import dataclass
+# fmt: off
 import os
 import sys
 
-
-@dataclass
 class OS:
-    rust_build: str
-    copy_lib: str
-    execute_python: str
+    def __init__(self):
+        self.rust_build = None
+        self.copy_lib = None
+        self.execute_python = None
+        self.compile_python = None
 
     def dev_build(self):
         os.system(self.rust_build)
         os.system(self.copy_lib)
         os.system(self.execute_python)
+    
+    def release_build(self):
+        os.system(self.rust_build)
+        os.system(self.copy_lib)
+        os.system(self.compile_python) # TODO: "compile" python
 
 
-@dataclass
 class Linux(OS):
-    rust_build: str = (
-        "cargo build --release --manifest-path=./modules/libkayn/Cargo.toml"
-    )
-    copy_lib: str = "cp ./modules/libkayn/target/release/liblibkayn.so ./libkayn.so"
-    execute_python: str = "python3 kayn.pyw"
-
-    def dev_build(self):
-        os.system(self.rust_build)
-        os.system(self.copy_lib)
-        os.system(self.execute_python)
+    def __init__(self):
+        self.rust_build = "cargo build --release --manifest-path=./modules/libkayn/Cargo.toml"
+        self.copy_lib = "cp ./modules/libkayn/target/release/liblibkayn.so ./libkayn.so"
+        self.execute_python = "python kayn.pyw"
 
 
 class Windows(OS):
-    rust_build: str = (
-        "cargo build --release --manifest-path=./modules/libkayn/Cargo.toml"
-    )
-    copy_lib: str = "copy modules\\libkayn\\target\\release\\libkayn.dll libkayn.pyd"
-    execute_python: str = "python kayn.pyw"
+    def __init__(self):
+        self.rust_build = "cargo build --release --manifest-path=./modules/libkayn/Cargo.toml"
+        self.copy_lib = "copy modules\\libkayn\\target\\release\\libkayn.dll libkayn.pyd"
+        self.execute_python = "python kayn.pyw"
+    
+class Mac(OS):
+    def __init__(self):
+        self.rust_build = "cargo build --release --manifest-path=./modules/libkayn/Cargo.toml"
+        self.copy_lib = "cp ./modules/libkayn/target/release/liblibkayn.dylib ./libkayn.so"
+        self.execute_python = "python kayn.pyw"
 
 
 if __name__ == "__main__":
@@ -48,4 +51,14 @@ if __name__ == "__main__":
         print("Unsupported OS")
         sys.exit(1)
 
-    system.dev_build()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "dev" or sys.argv[1] == "run":
+            system.dev_build()
+        elif sys.argv[1] == "release":
+            system.release_build()
+        else:
+            print("Unsupported argument")
+            sys.exit(1)
+    else:
+        print("No argument given. Available arguments: dev, release")
+        sys.exit(1)
